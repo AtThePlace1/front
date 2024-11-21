e5f72d4a18b35c6ba993aa1ed580c96f8ed309c8
Choongman Kim <cm730712@gmail.com>
2024-11-22 00:53:19 +0900

    [Feat] 카카오 로그인 기능 구현

---
+++ src/app/login/oauth/kakao/page.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { kakaoLogin } from '@/app/api/auth/kakao/kakaoLoginApi';
+
+export default function KaKaoRedirect() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleKakaoAuth = async () => {
+      const code = new URL(window.location.href).searchParams.get('code'); // URL에서 인가 코드 추출
+      console.log(code);
+      if (!code) {
+        console.error('카카오 인증 실패: 인가 코드가 없습니다.');
+        return;
+      }
+
+      try {
+        const data = await kakaoLogin(code); // 카카오 로그인 API 호출
+        if (data.token) {
+          localStorage.setItem('token', data.token); // 토큰 저장
+          router.push('/'); // 홈 페이지로 이동
+        }
+      } catch (error) {
+        console.error('카카오 로그인 실패:', error);
+      }
+    };
+
+    handleKakaoAuth();
+  }, []);
+
+  return <div>카카오 인증 중...</div>;
+}
